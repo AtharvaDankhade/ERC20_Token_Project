35e3c051bb90857b00c57d688466ff29cc8045d6
Atharva Dankhade <134969000+AtharvaDankhade@users.noreply.github.com>
2024-01-13 14:07:20 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,8 @@ require("@nomiclabs/hardhat-waffle");
 module.exports = {
   networks: {
     goerli: {
-      url: "https://goerli.infura.io/v3/fc53e1fe9181421188a85a7df27f9431", // Replace with your Infura project ID
-      accounts: [`b3944af79e8dc188eae5223908f4c3c9594a77b3cbe0b9c8130aa6bda879f11f`] // Replace with your private key
+      url: "1", // Replace with your Infura project ID
+      accounts: [``] // Replace with your private key
     }
   },
   solidity: "0.8.20",
